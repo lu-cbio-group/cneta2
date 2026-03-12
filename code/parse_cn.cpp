1a3717fa0d873f5d9ac6316deed9d283e15d8353
Ruolin W <93868179+lynnwrl@users.noreply.github.com>
2026-03-12 15:23:29 +0000

    Fix input validation in parse_cn
    Correct the isdigit() condition by fixing mismatched parentheses and ensuring all four columns are checked. Add a clearer error message that prints the offending line number and improve wording of the guidance text.

--- code/parse_cn.cpp
+++ code/parse_cn.cpp
@@ -523,10 +523,11 @@ void read_cn(vector<vector<vector<int>>>& s_info, const string& filename, int Ns
       while(ss >> buf) split.push_back(buf);
 
       const char* sstr = split[0].c_str();
-      if(!isdigit(*sstr) || !isdigit(*split[1].c_str()) || !isdigit(*split[2].c_str()  || !isdigit(*split[3].c_str()))){
+      if(!isdigit(*sstr) || !isdigit(*split[1].c_str()) || !isdigit(*split[2].c_str()) || !isdigit(*split[3].c_str())){
+          cout << "\nError: invalid format in line " << line << endl;
           cout << "Each column must be an integer!" << endl;
           cout << "The sample_ID has to be ordered from 1 to the number of patient samples." << endl;
-          cout << "The chr_ID and site_ID together determine a unique site along the genome of a sample, __ordering__ from 1 to the largest number." << endl;
+          cout << "The chr_ID and site_ID together determine a unique site along the genome of a sample, ordering from 1 to the largest number." << endl;
           cout << "The site_ID can be consecutive numbers from 1 to the total number of sites along the genome, or consecutive numbers from 1 to the total number of sites along each chromosome of the genome." << endl;
           cout << "For haplotype-specific CN, there need to be at least five columns, with the last two being cnA, cnB." << endl;
           exit(EXIT_FAILURE);
